785dcfe3282ef9cd93c72091eba053b50e47ca0e
Solar5503 <spidervvv@mail.ru>
2021-12-29 12:15:02 +0300

    Drag & Drop functionality

--- script.js
+++ script.js
@@ -37,6 +37,59 @@ function createList() {
       listItems.push(listItem);
       draggable_list.append(listItem);
     });
+
+  addEventListeners();
+}
+
+function dragStart() {
+  // console.log('Event: ', 'dragstart');
+  dragStartIndex = +this.closest('li').getAttribute('data-index');
+}
+
+function dragEnter() {
+  // console.log('Event: ', 'dragenter');
+  this.classList.add('over');
+}
+
+function dragLeave() {
+  // console.log('Event: ', 'dragleave');
+  this.classList.remove('over');
+}
+
+function dragOver(e) {
+  // console.log('Event: ', 'dragover');
+  e.preventDefault();
+}
+
+function dragDrop() {
+  // console.log('Event: ', 'drop');
+  const dragEndIndex = +this.closest('li').getAttribute('data-index');
+  swapItems(dragStartIndex, dragEndIndex);
+  this.classList.remove('over');
+}
+
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector('.draggable');
+  const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+  listItems[toIndex].append(itemOne);
+  listItems[fromIndex].append(itemTwo);
+}
+
+function addEventListeners() {
+  const draggables = document.querySelectorAll('.draggable');
+  const dragListItem = document.querySelectorAll('.draggable-list li');
+
+  draggables.forEach((draggable) =>
+    draggable.addEventListener('dragstart', dragStart)
+  );
+
+  dragListItem.forEach((item) => {
+    item.addEventListener('dragover', dragOver);
+    item.addEventListener('drop', dragDrop);
+    item.addEventListener('dragenter', dragEnter);
+    item.addEventListener('dragleave', dragLeave);
+  });
 }
 
 createList();
