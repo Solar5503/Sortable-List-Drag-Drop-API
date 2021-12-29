4014d0ddca816dd93c13eaefdc7205c09cbe838f
Solar5503 <spidervvv@mail.ru>
2021-12-29 15:25:19 +0300

    Check order

--- script.js
+++ script.js
@@ -68,12 +68,27 @@ function dragDrop() {
   this.classList.remove('over');
 }
 
+// Swap list items that are drag and drop
 function swapItems(fromIndex, toIndex) {
   const itemOne = listItems[fromIndex].querySelector('.draggable');
   const itemTwo = listItems[toIndex].querySelector('.draggable');
 
   listItems[toIndex].append(itemOne);
   listItems[fromIndex].append(itemTwo);
+}
+
+// Check the order of list items
+function checkOrder() {
+  listItems.forEach((listItem, index) => {
+    const personName = listItem.querySelector('.draggable').innerText.trim();
+
+    if (personName !== richestPeople[index]) {
+      listItem.classList.add('wrong');
+    } else {
+      listItem.classList.remove('wrong');
+      listItem.classList.add('right');
+    }
+  });
 }
 
 function addEventListeners() {
@@ -91,5 +106,7 @@ function addEventListeners() {
     item.addEventListener('dragleave', dragLeave);
   });
 }
+
+check.addEventListener('click', checkOrder);
 
 createList();
